711c9dd6199f3083b880bc1bc66b59d63442e894
Nanna <nanna.helge@gmail.com>
2026-03-02 18:49:46 +0100

    Added Assignment5 code template

--- Assignment4/obj/Debug/net9.0/Assignment4.AssemblyInfo.fs
+++ Assignment4/obj/Debug/net9.0/Assignment4.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Assignment4")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5770a5a68fd0fb1690958d7f77af718d5d105f6f")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f074b37aa5b97b00482085419914f3fb2ae4d4")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Assignment4")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Assignment4")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
